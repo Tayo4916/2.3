669307206e13176bce495be1fd7229b3840d3080
User <User@DESKTOP-KGVD8QQ>
2024-07-26 22:06:59 +0300

    Добавьте файлы проекта.

---
+++ 2.3.cpp
@@ -0,0 +1,16 @@
+﻿// 2.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int x, y, z;
+    z = 540 / 130;
+    cout << "В прямоугольнике 130х540 умещается " << floor(z) << " квадрата со стороной 130" << endl;
+    _getch();
+}
